a221bf6ad470b1c332a0dbe6da57ed42dd92298a
DogukanMermutlu <dogukanmermutlu@gmail.com>
2021-10-01 11:31:34 +0300

    Initial commit14

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -25,10 +25,8 @@ public class Base {
     public void beforeMethod() throws InterruptedException {
 
         /*Bu method login ol ve client olarak  */
-        System.setProperty("webdriver.chrome.driver", "/var/jenkins_home/driver/testdriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/var/jenkins_home/driver/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.setBinary("/var/jenkins_home/driver/chromedriver");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-setuid-sandbox");
         options.addArguments("--remote-debugging-port=9222");
@@ -37,6 +35,9 @@ public class Base {
         options.addArguments("--disable-gpu");
         options.addArguments("start-maximized");
         options.addArguments("disable-infobars");
+        options.addArguments("user-data-dir=/var/jenkins_home/driver/tmp/");
+        options.addArguments("headless");
+
         driver = new ChromeDriver(options);
         // options.addArguments("user-data-dir=/var/jenkins_home/driver/tmp/");
 
